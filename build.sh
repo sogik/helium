355873ca4cf70c616a9590f89f5078a72b90432e
sogik <itssogik@gmail.com>
2025-12-26 01:18:27 +0100

    update build.sh
    Added CCACHE support and updated build process.

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ export DEBIAN_FRONTEND=noninteractive
 echo ">>> Sistema detectado: Ubuntu ARM64 (Ampere)"
 sudo apt update
 sudo apt install -y sudo lsb-release file nano git curl python3 python3-pillow \
-    build-essential python3-dev libncurses5 openjdk-17-jdk-headless
+    build-essential python3-dev libncurses5 openjdk-17-jdk-headless ccache
 
 if [ ! -d "depot_tools" ]; then
     git clone --depth 1 https://chromium.googlesource.com/chromium/tools/depot_tools.git
@@ -69,14 +69,15 @@ python3 "${SCRIPT_DIR}/helium/utils/replace_resources.py" "${SCRIPT_DIR}/helium/
 
 echo ">>> Aplicando parches Helium..."
 if [ -d "$SCRIPT_DIR/helium/patches" ]; then
+    shopt -s nullglob
     for patch in $SCRIPT_DIR/helium/patches/*.patch; do
-        git apply --reject --whitespace=fix "$patch" || echo "⚠️ Conflicto parcial en $patch"
+        git apply --reject --whitespace=fix "$patch" || echo "Conflicto parcial en $patch"
     done
+    shopt -u nullglob
 fi
 
 sed -i 's/BASE_FEATURE(kExtensionManifestV2Unsupported, base::FEATURE_ENABLED_BY_DEFAULT);/BASE_FEATURE(kExtensionManifestV2Unsupported, base::FEATURE_DISABLED_BY_DEFAULT);/' extensions/common/extension_features.cc
 sed -i 's/BASE_FEATURE(kExtensionManifestV2Disabled, base::FEATURE_ENABLED_BY_DEFAULT);/BASE_FEATURE(kExtensionManifestV2Disabled, base::FEATURE_DISABLED_BY_DEFAULT);/' extensions/common/extension_features.cc
-
 sed -i '/<ViewStub/{N;N;N;N;N;N; /optional_button_stub/a\
 \
         <ViewStub\
@@ -85,15 +86,23 @@ sed -i '/<ViewStub/{N;N;N;N;N;N; /optional_button_stub/a\
             android:layout_width="wrap_content"\
             android:layout_height="match_parent" />
 }' chrome/browser/ui/android/toolbar/java/res/layout/toolbar_phone.xml
-
 sed -i 's/extension_toolbar_baseline_width">600dp/extension_toolbar_baseline_width">0dp/' chrome/browser/ui/android/extensions/java/res/values/dimens.xml
+
+export CCACHE_DIR=/home/$(whoami)/.ccache
+mkdir -p $CCACHE_DIR
+export CCACHE_MAXSIZE=30G 
+echo ">>> Usando CCache en: $CCACHE_DIR"
 
 cat > out/Default/args.gn <<EOF
 chrome_public_manifest_package = "io.github.jqssun.helium"
 is_desktop_android = true 
 target_os = "android"
 target_cpu = "arm64"
-host_cpu = "arm64"  # CRÍTICO: Indica que compilamos DESDE un ARM
+host_cpu = "arm64" 
+
+cc_wrapper = "ccache"
+use_thin_lto = false
+concurrent_links = 2
 
 is_component_build = false
 is_debug = false
@@ -113,7 +122,6 @@ google_api_key = "x"
 google_default_client_id = "x"
 google_default_client_secret = "x"
 
-concurrent_links = 2
 use_debug_fission=true
 use_errorprone_java_compiler=false
 use_official_google_api_keys=false
@@ -125,11 +133,12 @@ include_both_v8_snapshots_android_secondary_abi = false
 generate_linker_map = false
 EOF
 
-echo ">>> Compilando con Ninja..."
+echo ">>> Compilando..."
 gn gen out/Default
 autoninja -C out/Default chrome_public_apk
 
-export PATH=$PWD/third_party/jdk/current/bin/:$PATH
 export ANDROID_HOME=$PWD/third_party/android_sdk/public
 mkdir -p out/Default/apks/release
 sign_apk $(find out/Default/apks -name 'Chrome*.apk') out/Default/apks/release/$VERSION.apk
+
+ccache -s
